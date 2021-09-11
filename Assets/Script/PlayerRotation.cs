55640ae9c1c8c2adaaa0e884a8f321dfc5272095
matheus lobo <matheus.v.lobo@gmail.com>
2021-09-10 21:07:44 -0300

    V2 do projeto
    versão dois do projeto

---
+++ Assets/Script/PlayerRotation.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotation : MonoBehaviour
+{
+    public bool travarMouse = true;//controla se o cursor do mouse é exibido
+    public float sensibilidade = 2.0f;//controla a sensibilidade do mouse
+    private float mouseX = 0.0f, mouseY = 0.0f;//variavel q controla a rotação do mouse
+
+    void Start()
+    {
+        if (!travarMouse) {
+            return;
+        }
+        Cursor.visible = false; //oculta o cursor do mouse
+        Cursor.lockState = CursorLockMode.Locked;//trava o cursor no centro
+    }
+    
+    void Update()
+    {
+        mouseX += Input.GetAxis("MouseX") * sensibilidade;// Incrementa o valor do eixo X e multiplica pela sensibilidade
+        mouseY -= Input.GetAxis("MouseY") * sensibilidade;
+
+        transform.eulerAngles = new Vector3(mouseY, mouseX, 0);//Executa a rotação da câmera de acordo com os eixos
+    }
+}
